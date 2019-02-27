1b3ec755bd0487f2c0584f1a0c3ebc0e02f392f2
Daniel Kovac <dkovac@logitech.com>
2019-02-27 11:53:39 -0800

    Passing and retrieving data with action

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -17,12 +17,12 @@ const reducer = (state = initialState, action) => {
             break;
         case 'ADD':
             return {
-                counter: state.counter + 5
+                counter: state.counter + action.value
             }
             break;
         case 'SUBTRACT':
             return {
-                counter: state.counter - 5
+                counter: state.counter - action.value
             }
             break;
     }
